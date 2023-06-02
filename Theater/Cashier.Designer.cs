6c279f8109d988215a1a1edaaed69d434ed480c6
Veniamin2003 <veniapetrov941@gmail.com>
2023-06-03 00:02:02 +0500

    add cashier panel

--- Theater/Cashier.Designer.cs
+++ Theater/Cashier.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Theater
 {
-    partial class Login
+    partial class Cashier
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Login
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(394, 450);
-            this.Name = "Login";
-            this.Text = "Login";
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Cashier";
         }
 
         #endregion
     }
 }
-
